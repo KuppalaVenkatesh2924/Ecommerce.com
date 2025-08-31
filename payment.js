2673281df75ae35105793ce71018bfa7c7fde8cf
Kuppala Venkatesh <kuppalavenkatesh2924@gmail.com>
2025-08-31 17:31:36 +0530

    Delete payment.js

--- payment.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("payment-form").addEventListener("submit", e => {
-  e.preventDefault();
-  let method = document.querySelector("input[name='method']:checked").value;
-  localStorage.setItem("payment", method);
-  window.location.href = "success.html";
-});
